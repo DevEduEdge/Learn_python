85fe59fd6b82f5e14ed3552748b737eef1735455
NaN01WD <jammerking96@gmail.com>
2023-05-21 11:56:17 +0530

    Conditional statement

---
+++ Space_project.py
@@ -0,0 +1,13 @@
+parsecs = 11
+lightyears = parsecs * 3.26
+print(str(parsecs) + " parsecs is " + str(lightyears) + " lightyears")
+
+parsecs_input = int(input("Enter Parsecs number: "))
+parsecs = parsecs_input
+
+lightyears = 3.26156 * parsecs
+
+# print(parsecs)
+# print('Total parsecs are %d ' % parsecs)
+
+print(parsecs_input + " parsecs is " + str(lightyears) + " lightyears")
